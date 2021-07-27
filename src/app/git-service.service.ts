99f00cef159cbe031b3fa815974181690c067021
caseymusila <musilacasey@gmail.com>
2021-07-27 20:58:18 +0300

    we import our users from our module

--- src/app/git-service.service.ts
+++ src/app/git-service.service.ts
@@ -11,7 +11,7 @@ export class GitServiceService {
 
   user: Users[] = [];
   _URL = 'https://api.github.com/users/';
-  token = '?8b9e17143d094a525a75e0d0637d4f7691770cb4'
+  token = '?8b9e17143d094a525a75e0d0637d4f7691770cb4';
 
   constructor(private http: HttpClient) { }
 
@@ -33,7 +33,7 @@ export class GitServiceService {
 
         resolve();
       }, (err) => {
-        reject();
+        reject()
       }
       )
     })
